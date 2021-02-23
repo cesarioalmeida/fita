2e5d1c9acb8b498b6bb410feabe611bf8bb44292
Cesario Almeida <cesario.de.almeida@gmail.com>
2021-02-23 22:03:41 +0000

    added home view

---
+++ fita/fita.ui/ViewModels/Home/HomeViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using DevExpress.Mvvm.DataAnnotations;
+using twentySix.Framework.Core.UI.ViewModels;
+
+namespace fita.ui.ViewModels.Home
+{
+    [POCOViewModel]
+    public class HomeViewModel : ComposedViewModelBase
+    {
+        public async Task RefreshData()
+        {
+            IsBusy = true;
+
+            try
+            {
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
+    }
+}
