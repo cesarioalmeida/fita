7efa43f97fc5cd3983116a351c71141d5791c826
Cesario Almeida <cesario.de.almeida@gmail.com>
2021-03-02 12:39:31 +0000

    improved performance of comboboxes

---
+++ fita/fita.ui/Converters/EnumDisplayConverter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Linq;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace fita.ui.Converters
+{
+    public class EnumDisplayConverter : MarkupExtension, IValueConverter
+    {
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return this;
+        }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+            {
+                return DependencyProperty.UnsetValue;
+            }
+
+            var fieldInfo = value.GetType().GetField(value.ToString() ?? string.Empty);
+
+            if (fieldInfo == null)
+            {
+                return DependencyProperty.UnsetValue;
+            }
+            
+            var attributes = (DisplayAttribute[])fieldInfo.GetCustomAttributes(typeof(DisplayAttribute), false);
+            return attributes.ElementAt(0)?.Name ?? value.ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
