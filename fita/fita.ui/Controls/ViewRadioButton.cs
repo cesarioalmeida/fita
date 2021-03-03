c7d4c136a4644a25c2272b61a75e1fcb50f3bce4
Cesario Almeida <cesario.de.almeida@gmail.com>
2021-03-03 22:59:16 +0000

    improved performance and added transaction/portfolio buttons

---
+++ fita/fita.ui/Controls/ViewRadioButton.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace fita.ui.Controls
+{
+    public class ViewRadioButton : RadioButton
+    {
+        public static readonly DependencyProperty IconProperty = DependencyProperty.Register(
+            "Icon",
+            typeof(ImageSource),
+            typeof(ViewRadioButton),
+            new PropertyMetadata(null));
+
+        public static readonly DependencyProperty CaptionProperty = DependencyProperty.Register(
+            "Caption",
+            typeof(string),
+            typeof(ViewRadioButton),
+            new PropertyMetadata(string.Empty));
+
+        static ViewRadioButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(ViewRadioButton), new FrameworkPropertyMetadata(typeof(ViewRadioButton)));
+        }
+
+        public ImageSource Icon
+        {
+            get => (ImageSource)this.GetValue(IconProperty);
+            set => this.SetValue(IconProperty, value);
+        }
+
+        public string Caption
+        {
+            get => (string)this.GetValue(CaptionProperty);
+            set => this.SetValue(CaptionProperty, value);
+        }
+    }
+}
