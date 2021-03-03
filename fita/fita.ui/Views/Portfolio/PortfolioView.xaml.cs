d56612637728e1cb1abaaf8eb2c723e288dbba02
Cesario Almeida <cesario.de.almeida@gmail.com>
2021-03-03 23:18:32 +0000

    added portfolio view

---
+++ fita/fita.ui/Views/Portfolio/PortfolioView.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace fita.ui.Views.Portfolio
+{
+    public partial class PortfolioView
+    {
+        public PortfolioView()
+        {
+            InitializeComponent();
+        }
+    }
+}
