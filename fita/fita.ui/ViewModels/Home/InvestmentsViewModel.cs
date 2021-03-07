20ed68a2fc9986a46a8a32519c3af40f828d07ec
Cesario Almeida <cesario.de.almeida@gmail.com>
2021-03-07 15:40:19 +0000

    added investment view

---
+++ fita/fita.ui/ViewModels/Home/InvestmentsViewModel.cs
@@ -0,0 +1,96 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using DevExpress.Mvvm.DataAnnotations;
+using DevExpress.Xpf.Core;
+using fita.data.Enums;
+using fita.services.Core;
+using fita.services.Repositories;
+using twentySix.Framework.Core.UI.ViewModels;
+
+namespace fita.ui.ViewModels.Home
+{
+    [POCOViewModel]
+    public class InvestmentsViewModel : ComposedViewModelBase
+    {
+        public virtual LockableCollection<EntityModel> Data { get; set; } = new();
+
+        public virtual decimal TotalAmount { get; set; }
+
+        public virtual string BaseCulture {get; set;}
+
+        public AccountRepoService AccountRepoService { get; set; }
+
+        public TransactionRepoService TransactionRepoService { get; set; }
+
+        public FileSettingsRepoService FileSettingsRepoService { get; set; }
+
+        public SecurityPositionRepoService SecurityPositionRepoService { get; set; }
+
+        public SecurityHistoryRepoService SecurityHistoryRepoService { get; set; }
+
+        public IExchangeRateService ExchangeRateService { get; set; }
+
+        public IAccountService AccountService { get; set; }
+
+        public async Task RefreshData()
+        {
+            IsBusy = true;
+
+            Data.BeginUpdate();
+            Data.Clear();
+
+            TotalAmount = 0;
+
+            try
+            {
+                var accounts = await AccountRepoService.AllEnrichedAsync();
+                var baseCurrency = (await FileSettingsRepoService.AllEnrichedAsync()).First().BaseCurrency;
+                BaseCulture = baseCurrency.Culture;
+
+                foreach (var account in accounts.Where(x => x.Type == AccountTypeEnum.Investment))
+                {
+                    var transactions = (await TransactionRepoService.AllEnrichedForAccountAsync(account.AccountId)).ToList();
+                    var balance = await AccountService.CalculateBalance(account, transactions);
+                    Data.Add(new EntityModel(account.Name, account.Currency.Culture, balance));
+
+                    TotalAmount += await ExchangeRateService.Exchange(account.Currency, baseCurrency, balance);
+
+                    // positions
+                    var positions = await SecurityPositionRepoService.AllEnrichedForAccountAsync(account.AccountId);
+                    var totalPositions = 0m;
+
+                    foreach (var position in positions)
+                    {
+                        totalPositions += ((await SecurityHistoryRepoService.FromSecurityEnrichedAsync(position.Security))?
+                            .Price?.LatestValue ?? 0m) * position.Quantity;
+                    }
+
+                    Data.Add(new EntityModel(account.Name + " - Portfolio", account.Currency.Culture, totalPositions));
+
+                    TotalAmount += await ExchangeRateService.Exchange(account.Currency, baseCurrency, totalPositions);
+                }
+            }
+            finally
+            {
+                Data.EndUpdate();
+                IsBusy = false;
+            }
+        }
+
+        public class EntityModel
+        {
+            public EntityModel(string accountName, string accountCulture, decimal balance)
+            {
+                Name = accountName;
+                Balance = balance;
+                Culture = accountCulture;
+            }
+
+            public string Name { get; }
+
+            public decimal Balance { get; }
+
+            public string Culture { get; }
+        }
+    }
+}
