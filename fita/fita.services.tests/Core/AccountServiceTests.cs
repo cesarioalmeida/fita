bb05097f0b47aa40132ac8f5a794306cf80bf95a
Cesario Almeida <cesario.de.almeida@gmail.com>
2021-05-29 12:06:46 +0100

    added unit tests around the account service

---
+++ fita/fita.services.tests/Core/AccountServiceTests.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using fita.data.Models;
+using fita.services.Core;
+using fita.tests.common;
+using NUnit.Framework;
+
+namespace fita.services.tests.Core
+{
+    [TestFixture]
+    public class AccountServiceTests : ContainerFixture
+    {
+        private IAccountService _accountService;
+
+        [Test]
+        public async Task CalculateBalance_AccountNull_Returns0()
+        {
+            Assert.AreEqual(0m, await _accountService.CalculateBalance(null, null));
+        }
+
+        [Test]
+        public async Task CalculateBalance_AccountNoTransactions_ReturnsInitialBalance()
+        {
+            var account = new Account {InitialBalance = 10m};
+
+            Assert.AreEqual(10m, await _accountService.CalculateBalance(account, null));
+        }
+
+        [Test]
+        public async Task CalculateBalance_OnlyDepositTransactions_ReturnsBalance()
+        {
+            var account = new Account {InitialBalance = 10m};
+            var transactions = new[]
+            {
+                new Transaction {Deposit = 2m},
+                new Transaction {Deposit = 3m},
+                new Transaction {Deposit = 10m},
+            }.ToList();
+
+            Assert.AreEqual(25m, await _accountService.CalculateBalance(account, transactions));
+        }
+
+        [Test]
+        public async Task CalculateBalance_OnlyPaymentTransactions_ReturnsBalance()
+        {
+            var account = new Account {InitialBalance = 10m};
+            var transactions = new[]
+            {
+                new Transaction {Payment = 2m},
+                new Transaction {Payment = 3m},
+                new Transaction {Payment = 3m},
+            }.ToList();
+
+            Assert.AreEqual(2m, await _accountService.CalculateBalance(account, transactions));
+        }
+
+        [Test]
+        public async Task CalculateBalance_MixTransactions_ReturnsBalance()
+        {
+            var account = new Account {InitialBalance = 10m};
+            var transactions = new[]
+            {
+                new Transaction {Deposit = 2m},
+                new Transaction {Deposit = 3m},
+                new Transaction {Payment = 10m},
+            }.ToList();
+
+            Assert.AreEqual(5m, await _accountService.CalculateBalance(account, transactions));
+        }
+
+        [Test]
+        public async Task CalculateBalance_OutOfOrderTransactions_ReturnsBalance()
+        {
+            var account = new Account {InitialBalance = 10m};
+            var transactions = new[]
+            {
+                new Transaction {Date = DateTime.Today.AddDays(-9), Deposit = 2m},
+                new Transaction {Date = DateTime.Today.AddDays(-1), Deposit = 3m},
+                new Transaction {Date = DateTime.Today.AddDays(-10), Payment = 10m},
+            }.ToList();
+
+            Assert.AreEqual(5m, await _accountService.CalculateBalance(account, transactions));
+        }
+
+        [Test]
+        public async Task CalculateBalance_TransactionNull_ReturnsBalance()
+        {
+            Assert.AreEqual(10m, await _accountService.CalculateBalance(null, 10m));
+        }
+
+        [Test]
+        public async Task CalculateBalance_DepositTransaction_ReturnsBalance()
+        {
+            var transaction = new Transaction {Deposit = 2m};
+            Assert.AreEqual(12m, await _accountService.CalculateBalance(transaction, 10m));
+        }
+        
+        [Test]
+        public async Task CalculateBalance_PaymentTransaction_ReturnsBalance()
+        {
+            var transaction = new Transaction {Payment = 2m};
+            Assert.AreEqual(8m, await _accountService.CalculateBalance(transaction, 10m));
+        }
+    }
+}
